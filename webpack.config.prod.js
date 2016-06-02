35ed040e5c3eeb20a92fab9c0f3b9ab81f79f04a
Andreas Argelius <andreas@argeli.us>
2016-06-02 13:14:57 +0900

    Add production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  context: __dirname,
+  entry: [
+    './index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      { test: /\.js$|\.jsx$/,
+        exclude: [/node_modules/],
+        loaders: [
+          'react-hot',
+          'babel?' + JSON.stringify({presets: ['stage-2', 'es2015', 'react']})
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
+
