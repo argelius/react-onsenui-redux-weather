962b84c46f3f0328e51c553a762f5da743b85bcd
Andreas Argelius <andreas@argeli.us>
2016-06-08 20:45:04 +0900

    Deploy

--- dist/bundle.js
+++ dist/bundle.js
@@ -99,7 +99,7 @@
 	  return f;
 	},  true ? (0, _redux.applyMiddleware)(_reduxThunk2.default) : (0, _redux.applyMiddleware)(_reduxThunk2.default, logger));
 	
-	['Tokyo', 'New York', 'Paris', 'Beijing', 'Sydney', 'Buenos Aires'].forEach(function (city) {
+	['Tokyo', 'New York', 'Paris', 'Beijing', 'Sydney', 'Sao Paolo'].forEach(function (city) {
 	  return store.dispatch((0, _actions.addLocationAndFetchWeather)(city));
 	});
 	
